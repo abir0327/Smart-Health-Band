ce2c990ee2cea22f5c1c0d7f2bedbee5d75d44a8
abir0327 <125345704+abir0327@users.noreply.github.com>
2023-02-16 15:07:09 +0600

    Add files via upload

---
+++ gps2/gps2.ino
@@ -0,0 +1,58 @@
+
+
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID           "demo1"
+#define BLYNK_DEVICE_NAME           "NodeMCU"
+#define BLYNK_AUTH_TOKEN            "gpgK0Uiu8UWEvpEkZ2tH4Zsf9Q-QloH1"
+
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Galaxy M315087";
+char pass[] = "sfto0587";
+
+BLYNK_WRITE(V1) {
+  GpsParam gps(param);
+
+  // Print 6 decimal places for Lat, Lon
+  Serial.print("Lat: ");
+  Serial.println(gps.getLat(), 7);
+
+  Serial.print("Lon: ");
+  Serial.println(gps.getLon(), 7);
+
+  // Print 2 decimal places for Alt, Speed
+  Serial.print("Altitute: ");
+  Serial.println(gps.getAltitude(), 2);
+
+  Serial.print("Speed: ");
+  Serial.println(gps.getSpeed(), 2);
+
+  Serial.println();
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+}
